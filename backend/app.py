a4a555616c5d00e4425e48bc5a3fe78ff13bfda8
DatPhanGH <156731929+DatPhanGH@users.noreply.github.com>
2025-05-18 21:17:26 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, render_template
 from api.predict import predict_clap
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -23,4 +24,6 @@ def predict():
     return jsonify({'result': result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0', port = port)
